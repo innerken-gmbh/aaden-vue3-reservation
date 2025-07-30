5cb14eebe54ec0be98756931dc51fe0033e739de
噢嚯 <88487061+513355236@users.noreply.github.com>
2025-07-30 17:03:38 +0800

    Base换成Key

--- src/main.js
+++ src/main.js
@@ -8,20 +8,37 @@ import './styles/main.scss';
 import IKUtils from "innerken-js-utils";
 import dayjs from "dayjs";
 import {i18n} from "./plugins/i18n.js";
-import hillo from "hillo";
 
 
 
-const id = IKUtils.getQueryString("userId") || 1
-const base = IKUtils.getQueryString("Base")
-export async function getDeviceId() {
-    if (base) {
-        return (await hillo.post(location.protocol + '//' + base + '/PHP/AccessLog.php?op=deviceId', {}))?.content
-    } else {
-        return false
+const id = IKUtils.getQueryString("userId")
+const keywords = IKUtils.getQueryString("Key")
+
+function decryptCode(code) {
+    let originalStr = '';
+    for (let i = 0; i < code.length; i++) {
+        const char = code[i];
+        const charCode = char.charCodeAt(0) - 65; // 0-25
+        // 反向计算数字值
+        const digit = (charCode - i * 3 + 26) % 26;
+        originalStr += digit.toString();
     }
+
+    // 去除前导0并转为数字
+    return parseInt(originalStr);
 }
-export const userId = base ? parseInt(await getDeviceId()) : parseInt(id)
+
+export function getDeviceId() {
+    if (id) {
+        return id
+    }
+    if (keywords) {
+        return decryptCode(keywords)
+    }
+    return 1
+}
+
+export const userId = getDeviceId()
 dayjs.locale(Remember.lang)
 
 createApp(App)
